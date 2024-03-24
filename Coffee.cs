f554d3b3a3e304939f6a9f266278b053bfb337d3
Gubynator <goubiah@gmail.com>
2024-03-24 10:45:17 -0600

    CoffeeShopApp version 1

---
+++ Coffee.cs
@@ -0,0 +1,14 @@
+using System;
+
+namespace CoffeeShop
+{
+    public class Coffee
+    {
+        public string? Name {get; set;}
+        public int Strength {get; set;}
+        public string? Bean {get; set;}
+        public int size {get; set;}
+        public double Price {get; set;}
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using CoffeeShop;
+
+namespace CoffeeShopApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Coffee coffee = PrepareStandardCoffee();
+
+            PrintCoffeDetails(coffee);
+        }
+        static public Coffee PrepareStandardCoffee()
+        {
+            Coffee coffee = new()
+            {
+                Name = "Espresso",
+                Strength = 5,
+                Bean = "Arabica",
+                size = 12,
+                Price = 2.99
+            };
+
+            return coffee;
+        }
+
+        static public void PrintCoffeDetails(Coffee coffee)
+        {
+            Console.WriteLine($"Name: {coffee.Name}");
+            Console.WriteLine($"Strength: {coffee.Strength}");
+            Console.WriteLine($"Bean: {coffee.Bean}");
+            Console.WriteLine($"Size: {coffee.size}");
+            Console.WriteLine($"Price: {coffee.Price}");
+        }
+    }
+}
+
